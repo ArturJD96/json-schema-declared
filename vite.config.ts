49295763aab1793b99ad3e35b8cb4034c9394651
Artur J. Dobija <arturdobija@gmail.com>
2026-08-18 14:41:33 +0200

    Updated config

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,9 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   build: {
     lib: {
-      entry: './lib/main.ts',
+      entry: './lib/main.d.ts',
       formats: ['es'],
-      fileName: 'counter',
+      fileName: 'json-schema-metaschemas',
     },
   },
 })
