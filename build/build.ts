7a9e19e4d02501f23cc22dc80a90515277efc882
Artur J. Dobija <arturdobija@gmail.com>
2026-08-19 13:16:24 +0200

    Added description.

--- build/build.ts
+++ build/build.ts
@@ -1,3 +1,8 @@
+/**
+ * This script downloads JSON Schemas specified in `metaschemas.json`
+ * and builds `declarations.d.ts` file with Typescript declarations.
+ */
+
 import { rmDirs } from "./utils";
 import { downloadMetaschemas } from "./metaschemas";
 import { createDeclarationFiles } from "./declarations";
